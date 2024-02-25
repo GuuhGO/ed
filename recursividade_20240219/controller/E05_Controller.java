351900a340522e5beeaa30f912a4449ff229e088
Gustavo Guimarães <gu.guimaraesrdc9@gmail.com>
2024-02-24 21:45:17 -0300

    Exercício 5 finalizado

---
+++ recursividade_20240219/controller/E05_Controller.java
@@ -0,0 +1,27 @@
+/*5. Criar uma aplicação em Java que tenha uma função recursiva que, recebendo um número
+inteiro (N), apresente a saída da somatória
+S = 1 + 1/2 + 1/3 + 1/4 + ... + 1/N
+• O Código deve apresentar, em formato de comentário, como foi definida a condição
+de parada;
+• O Código deve apresentar, em formato de comentário, como foi definida a relação de
+chamada dos passos;*/
+package ed.recursividade_20240219.controller;
+
+public class E05_Controller {
+	public E05_Controller() {
+		super();
+	}
+	public double somatoria(int n) {
+		// CONDIÇÃO DE PARADA
+		// Se n for maior que 1, calcula a divisão de 1 por n e armazena em div
+		// o resultado de div é somado com o resultado da próxima chamada da
+		// função que recebe n - 1 como parâmetro e depois 
+		// retorna-se o valor da soma.
+		if (n > 1) {
+			double div = (double) 1 / (double) n;
+			return div + somatoria(n - 1);
+		}
+		// Quando N for igual a 1, retorna-se N pois 1/1 = 1
+		return (double) n;
+	}
+}
